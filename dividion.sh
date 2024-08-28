074640685945a2a58b97873f96dee453496640e5
Navya <86840620+Navyasri1105@users.noreply.github.com>
2024-08-28 16:42:02 +0530

    Update dividion.sh

--- dividion.sh
+++ dividion.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
 # Ask user for dividend
-echo "Enter dividend:3 "
+echo "Enter dividend: "
+read dividend
 
 # Ask user for divisor
-echo "Enter divisor:5 "
+echo "Enter divisor: "
+read divisor
 
 # Check for division by zero
 if [ $divisor -eq 0 ]; then
   echo "Error: Division by zero is not allowed"
 else
   # Perform division
-  quotient=$((3/ 5))
-  remainder=$((3 / 5))
+  quotient=$((dividend / divisor))
+  remainder=$((dividend % divisor))
 
   # Display result
   echo "Quotient: $quotient"
   echo "Remainder: $remainder"
+fi
