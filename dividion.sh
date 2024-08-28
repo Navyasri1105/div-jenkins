4ffb37aef19169da33c0681bdd29987137c16e1b
navya <jenkins@123gmail.com>
2024-08-28 16:33:34 +0530

    hi

---
+++ dividion.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Ask user for dividend
+echo "Enter dividend:3 "
+
+# Ask user for divisor
+echo "Enter divisor:5 "
+
+# Check for division by zero
+if [ $divisor -eq 0 ]; then
+  echo "Error: Division by zero is not allowed"
+else
+  # Perform division
+  quotient=$((3/ 5))
+  remainder=$((3 % 5))
+
+  # Display result
+  echo "Quotient: $quotient"
+  echo "Remainder: $remainder"
